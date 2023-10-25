4f9fdb69b7be2d0bf66deed5a3fcd324b9d6d4b4
ANGEL BELANGEL <125672230+gellybbbb@users.noreply.github.com>
2023-10-25 12:03:57 +0800

    Update index .ts

--- index .ts
+++ index .ts
@@ -1,55 +1,69 @@
-const axios = require('axios');
 
-async function fetchData() {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-    console.log('GET Response:', response.data);
-  } catch (error) {
-    console.error('GET Error:', error);
-  }
-}
+const axios = require('axios'); 
+                                        /* GET REQUEST */
+// async function fetch() {
+//   try {
+//     const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-async function postData() {
+
+
+                                         /* POST REQUEST */
+// async function post() {
+//     try {
+//      const data = {
+//            userId: 1,
+//             id: 101,
+//             title: 'hello world',
+//             completed: false
+//           };        
+
+//             const response = await axios.post('https://jsonplaceholder.typicode.com/todos', data);
+//             console.log(response.data);
+//           } catch (error) {
+//             console.error( error);
+//           }
+//         }
+                            
+                    
+
+                                          /* PUT REQUEST*/
+
+async function update() {
   try {
     const data = {
-      userId: 1,
-      id: 101,
-      title: 'Sample Title',
-      completed: false
-    };
-
-    const response = await axios.post('https://jsonplaceholder.typicode.com/todos', data);
-    console.log('POST Response:', response.data);
-  } catch (error) {
-    console.error('POST Error:', error);
-  }
-}
-
-async function updateData() {
-  try {
-    const data = {
-      title: 'Updated Title',
+      title: 'HELLO WORLD',
       completed: true
     };
-
-    const response = await axios.put('https://jsonplaceholder.typicode.com/todos/1', data);
-    console.log('PUT/PATCH Response:', response.data);
+     const response = await axios.put('https://jsonplaceholder.typicode.com/todos/1', data);
+    console.log(response.data);
   } catch (error) {
-    console.error('PUT/PATCH Error:', error);
+    console.error(error);
   }
 }
 
-async function deleteData() {
-  try {
-    const response = await axios.delete('https://jsonplaceholder.typicode.com/todos/1');
-    console.log('DELETE Response Status:', response.status);
-  } catch (error) {
-    console.error('DELETE Error:', error);
-  }
-}
 
-// Usage
-fetchData();
-postData();
-updateData();
-deleteData();
+
+
+
+                                    /* DELETE REQUEST*/
+// async function delete() {
+//   try {
+//     const response = await axios.delete('https://jsonplaceholder.typicode.com/todos/1');
+//     console.log(response.status);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+  
+
+
+//fetch();
+// post();
+update();
+// delete();
